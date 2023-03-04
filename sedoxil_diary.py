6f00e4dc477ace4909904062011358d67903fa31
Sasha <sashalisina@gmail.com>
2023-03-04 12:09:05 +0000

    Update sedoxil_diary.py

--- sedoxil_diary.py
+++ sedoxil_diary.py
@@ -7,14 +7,22 @@ import sys
 
 
 today = date.today()
-today =str(today.strftime("%Y/%m/%d"))
-
+today =today.strftime("%Y/%m/%d")
 def main():
+    """
+    Creates a diary file if a user doesn't have one yet or open the existing one and gets the variables for making an observation for this day.
+
+    :param :
+    :type of input :
+    :raise FileNotFoundError: If there is no a csv file.
+    :return:
+    :rtype:
+    """
     print()
     print('WELCOME TO SEDOXIL DIARY!   We need to make some observations !\n')
     f=open("diary.csv")
-    csv_dict = [row for row in csv.DictReader(f)]
-    if len(csv_dict) == 0:
+    csv_list_of_dicts = [row for row in csv.DictReader(f)]
+    if len(csv_list_of_dicts) == 0:
         print("You have not created a diary file for you yet. let's do it now!\n")
         f.close
 
@@ -27,14 +35,13 @@ def main():
             print("Have a nice trip!")
     else:
         with open("diary.csv") as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                daynote = row
-                starting_date = daynote["starting_date"]
-                days_of_treatment = int(daynote["days_of_treatment"])
+            list_of_diaries=[row for row in csv.DictReader(f)]
+            starting_date, days_of_treatment = list_of_diaries[0]["starting_date"],int(list_of_diaries[0]["days_of_treatment"])
+            print (starting_date, days_of_treatment)
+            for daynote in list_of_diaries:
                 date_today = daynote["date_today"]
                 if date_today == today:
-                    print('You already filled for today! Enjoy!')
+                    print('You have already filled for today! Enjoy!')
                     sys.exit(0)
 
 
@@ -42,12 +49,21 @@ def main():
     fill_daynote(newdaynote)
 
 def get_days_of_treatment():
+    """
+    Receives from the user the duration of the treatment.
+
+    :param : Gets the user's input
+    :type of input: int (expected)
+    :raise ValueError: If user's input is not an int < 100
+    :return: A number of day for treatment
+    :rtype: int
+    """
     while True:
         try:
             howlong = input("How many do days you need to take a treatment? ")
             print()
-            matches = re.search(r"^([0-9]{1,2})$", howlong)
-            if matches:
+            matches = re.search(r"^([0-9]{1,2})$", howlong) #re looks in an input for desimals up to two numbers
+            if matches: # if it finds, it convert this to int and saves in a variable
                 days_of_treatment = int(matches.group(1))
             else:
                 raise ValueError
@@ -59,27 +75,32 @@ def get_days_of_treatment():
 
 
 def get_starting_day():
+    """
+    Receives from the user the date she/he started the treatment.
+
+    :param : Gets the user's input
+    :type n: str
+    :raise ValueError: If an input doesn't look like a valid date and :raise TypeError: if date is in futere.
+    :return: A string of a date like YYYY/MM/DD
+    :rtype: str
+    """
     while True:  # getting from a user a date of starting the treatment.
         try:
             starting_date = input("What was the date you started to take Sedoxil?(MM/DD): 2023/ ").strip()
-            print()
             matches = re.search(r"^([0-9]{1,2})\D+([0-9]{1,2}$)", starting_date)
             if matches:
                 if (int(matches.group(1))>12) or (int(matches.group(2))> 31):
                     raise ValueError
                 starting_date = ('2023/' + str(matches.group(1)) + '/' + str(matches.group(2)))
-                print(type(starting_date))
-                start_date = datetime.strptime(starting_date,"%Y/%m/%d")
-                now_date = datetime.strptime(today,"%Y/%m/%d")
+
                 if start_date > now_date:
-                    #print(f"{start_date}>{now_date}")
-                    raise ArithmeticErrorError
+                    raise TypeError
             else:
                 raise ValueError
         except ValueError:
             print()
             print("The date is not recognized, please use the decimal numbers up to 12 for months and up to 31 for days \n")
-        except ArithmeticError:
+        except TypeError:
             print()
             print("You wrote the date in the future as the starting date, but you can't start the diary about the future! :) Sorry \n")
         else:
@@ -90,6 +111,15 @@ def get_starting_day():
 
 
 def progress(starting_date, today, days_of_treatment):
+    """
+    Gets a progress in the timeline of treatment.
+
+    :param n: the day of starting the treatment and duration
+    :type n: str and int
+    :raise TypeError: If n is not a str or an int
+    :return: the day of treatment and how many day left.
+    :rtype: int or False
+    """
     start_date = datetime.strptime((starting_date),"%Y/%m/%d")
     now_date = datetime.strptime(today,"%Y/%m/%d")
     delta = now_date - start_date
@@ -106,6 +136,15 @@ def progress(starting_date, today, days_of_treatment):
     return (Nday_of_treatment, days_of_treatment_left)
 
 def fill_for_theday(starting_date, date, days_of_treatment):
+    """
+    Provides a user a questionnaire to fill the day observations and saves it in a dictionary.
+
+    :param n: Number of times to meow
+    :type n: int
+    :raise ValueError: If the user input is not in the range of the expected response
+    :return: A string of n meows, one per line
+    :rtype: dictionary
+    """
     date_today = date
     print()
     Nday_of_treatment,days_of_treatment_left  = progress(starting_date, date_today, days_of_treatment)
@@ -237,12 +276,21 @@ def fill_for_theday(starting_date, date, days_of_treatment):
     return (newdaynote)
 
 def fill_daynote(newdaynote):
+    """
+    Fills the row in a diary csv file with day observations.
+
+    :param n: dictionary
+    :type n: dict
+    :raise FileNotFoundError: If there is no a csv file.
+    :return:
+    :rtype:
+    """
     with open ("diary.csv", "a+", newline='') as f:
        writer = csv.DictWriter(f, fieldnames=["date_today", "starting_date", "days_of_treatment","Nday_of_treatment","days_of_treatment_left",
                                               "sleep_state","morning_state","morning_pill","second_pill_time","time_reason_for2pill", "printreport"])
        writer.writerow(newdaynote)
 
-    if newdaynote["printreport"] == True: # printreport
+    if newdaynote["printreport"] == True: # print final report roe a docto in a separete csv file.
         print()
         print(f"Thank for filling information for the day {newdaynote['date_today']}. Now it's recorded in your file. "
           "It's you last day of treatment and we print a report for your doctor. You can find it in the same directory named doctor_report.csv \n")
@@ -269,75 +317,79 @@ def fill_daynote(newdaynote):
 
 
 def fill_empty(starting_date, days_of_treatment):
-    dates=[]
+    """
+    Finds the gap dates(unfilled) in diary and creates the empty dictionary for the empty line in a doctor report.
+    :param n: Starting date of treatment, number of days for treatment.
+    :type n: str, int
+    :raise FileNotFoundError: If there is no a csv file.
+    :return: A string of n meows, one per line
+    :rtype: list of dictionaries with empty perorts for that days.
+    """
     with open("diary.csv") as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                date = row["date_today"]
-                dates.append(date)
-    print(f'dates filled in diary - {dates}')
+        dates = [row["date_today"] for row in csv.DictReader(f)]   # creates a list of dates whith the observations were completed.
 
-    start= datetime.strptime(starting_date,"%Y/%m/%d")
-    list_of_dates= []
-    for i in range( days_of_treatment +1):
-        next_day = start + timedelta(days = i)
-        list_of_dates.append(str(next_day).strip('00:00:00').strip())
-    print(f'the period  of dates to fill {list_of_dates}')
+    #print(f'dates filled in diary - {dates}')
 
-    gap_dates=[]
-    for date in list_of_dates:
-        if date not in dates:
-            gap_dates.append(date)
-    print(gap_dates)
+    start= datetime.strptime(starting_date,"%Y/%m/%d")  #converts the first day of treatment into timedate format as it comes as str.
 
-    gap_reports=[]
-    for date in gap_dates:
-        missed_daynote = {}
-        missed_daynote["date_today"] = date
-        missed_daynote["starting_date"]='-'
-        missed_daynote["days_of_treatment"]='-'
-        missed_daynote["Nday_of_treatment"]='-'
-        missed_daynote["days_of_treatment_left"]='-'
-        missed_daynote["sleep_state"]='-'
-        missed_daynote["morning_state"]='-'
-        missed_daynote["morning_pill"]='-'
-        missed_daynote["second_pill_time"]='-'
-        missed_daynote["time_reason_for2pill"]= '-'
-        missed_daynote["printreport"] = False
-        gap_reports.append(missed_daynote)
-    #print (gap_reports)
+    list_of_dates_to_fill=[(start + timedelta(days = i)).strftime("%Y/%m/%d") for i in range( days_of_treatment)] # creates a list ofall dates during the
+                                                                                                                   #treatment period in str format(.strftime("%Y/%m/%d"))
+    #print(f'list_of_dates_to_fill {list_of_dates_to_fill}')
+
+    gap_dates = [date for date in list_of_dates_to_fill if date not in dates] #creates the list af dates when the ovservations were not made.
+    #print(f'gap_dates {gap_dates}')
+
+    gap_reports=[{"date_today" : date, "starting_date":'-',"days_of_treatment": '-',"Nday_of_treatment":'-', "days_of_treatment_left":'-', "sleep_state": '-',
+             "morning_state":'-', "morning_pill":'-', "second_pill_time":'-', "time_reason_for2pill":'-',"printreport": False} for date in gap_dates]
+    #create unfilled day observations with a sertain date.
     return gap_reports
 
 def print_report_for_a_doctor():
-    reports=[]
+    """
+    Creates a final report for a doctor for the whole treatment period including unfilled dates.
+
+    :param n: no
+    :type n:
+    :raise FileNotFoundError: If there is no a csv file.
+    :return:
+    :rtype:
+    """
+
     with open("diary.csv") as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                daynote = row
-                reports.append(daynote)
-    starting_date, days_of_treatment = reports[0]['starting_date'], reports[0]['days_of_treatment']
-    gap_reports= fill_empty(starting_date, days_of_treatment)
-    wholereport= (reports+gap_reports)
-    #print (wholereport)
-    wholereport = sorted(wholereport, key=lambda daynote: daynote["date_today"])
+        reports = [row for row in csv.DictReader(f)]                                                        #gets the information for filled days
+    starting_date, days_of_treatment = reports[0]['starting_date'], reports[0]['days_of_treatment']     #retrieves the arguments for fill_empty function.
+    gap_reports= fill_empty(starting_date, days_of_treatment)                                           #creates missed reports.
+    wholereport = sorted((reports+gap_reports), key=lambda daynote: daynote["date_today"])              #combines reports together sorted by date.
     #print (wholereport)
 
-    for daynote in  wholereport:
-        with open ("doctor_report.csv", "a+", newline='') as f:
+    for daynote in  wholereport:                                                                        # writes a doctor report
+        with open ("doctor_report.csv", "w", newline='') as f:
             writer = csv.DictWriter(f, fieldnames=["date_today","starting_date","days_of_treatment","Nday_of_treatment","days_of_treatment_left",
                                               "sleep_state","morning_state","morning_pill","second_pill_time","time_reason_for2pill","printreport"])
             writer.writerow(daynote)
 
 def summary(newdaynote):
+    """
+    Shows in a simple table the information that was just provided and gives the user the chance to change it.
 
-        print(tabulate(newdaynote.items()))
-        print()
-        starting_date = newdaynote['starting_date']
-        days_of_treatment= newdaynote['days_of_treatment']
-        date_today = newdaynote['date_today']
-        change_the_note  = input("Do you want to change you note? Type 'yes' or 'no' : ").strip().upper()
-        if change_the_note in ['Y', 'Yes', 'OK']:
-                fill_for_theday(starting_date,date_today,days_of_treatment,)
+    :param n: dictionary with filled information about the day observation.
+    :type n: dictionary
+    :raise ValueError: If the user input is not in the range of the expected response
+    :return:
+    :rtype:
+    """
+    print(tabulate(newdaynote.items())) # draw a table with new day observation.
+    print()
+    while True:
+        try:
+            change_the_note  = input("Do you want to change you note? Type 'yes' or 'no' : ").strip().upper()
+            if change_the_note in ['Y', 'Yes', 'OK']:
+                fill_for_theday(newdaynote['starting_date'],newdaynote['date_today'],newdaynote['days_of_treatment'])
+
+        except ValueError:
+            print('ok')
+        else:
+           break
 
 if __name__ == "__main__":
     main()
