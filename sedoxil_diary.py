fe3eebafe8b3657c2eac6a52da77e3eb14413824
Sasha <sashalisina@gmail.com>
2023-03-11 11:55:50 +0000

    Update sedoxil_diary.py

--- sedoxil_diary.py
+++ sedoxil_diary.py
@@ -85,29 +85,29 @@ def get_starting_day():
     :rtype: str
     """
     while True:  # getting from a user a date of starting the treatment.
-    try:
-        starting_date = input("What was the date you started to take Sedoxil?(MM/DD): 2023/ ").strip()
-        matches = re.search(r"^([0-9]{1,2})\D*([0-9]{1,2}$)", starting_date)
-        if matches:
-            print(f"matches.group(1){matches.group(1)},matches.group(2){matches.group(2)}" )
+        try:
+            starting_date = input("What was the date you started to take Sedoxil?(MM/DD): 2023/ ").strip()
+            #matches = re.search(r"^([0-9]{1,2})\D*([0-9]{1,2}$)", starting_date)
+            if matches:
+                print(f"matches.group(1){matches.group(1)},matches.group(2){matches.group(2)}" )
 
-            if (int(matches.group(1))< 12) or (int(matches.group(1)) > 0 ) and ((int(matches.group(2)) < 31) or (int(matches.group(2)) > 0)) :
+                if (int(matches.group(1)) > 12) or (int(matches.group(1)) < 0 ) and ((int(matches.group(2)) > 31) or (int(matches.group(2)) < 0)) :
+                    raise ValueError
+                starting_date = ('2023/' + str(matches.group(1)) + '/' + str(matches.group(2)))
+
+                if starting_date > today:
+                    raise TypeError
+            else:
                 raise ValueError
-            starting_date = ('2023/' + str(matches.group(1)) + '/' + str(matches.group(2)))
 
-            if starting_date > today:
-                raise TypeError
+        except ValueError:
+           print()
+           print("The date is not recognized, please use the decimal numbers up to 12 for months and up to 31 for days and bigger than 0 :)\n")
+        except TypeError:
+           print()
+           print("You wrote the date in the future as the starting date, but you can't start the diary about the future! :) Sorry \n")
         else:
-            raise ValueError
-
-    except ValueError:
-        print()
-        print("The date is not recognized, please use the decimal numbers up to 12 for months and up to 31 for days and bigger than 0 :)\n")
-    except TypeError:
-        print()
-        print("You wrote the date in the future as the starting date, but you can't start the diary about the future! :) Sorry \n")
-    else:
-        break
+            break
     return starting_date
 
 
@@ -150,6 +150,7 @@ def fill_for_theday(starting_date, date, days_of_treatment):
     """
     date_today = date
     print()
+    print(f'Hello! today is {date_today}')
     Nday_of_treatment,days_of_treatment_left  = progress(starting_date, date_today, days_of_treatment)
     printreport = False
     if days_of_treatment_left == 0:
