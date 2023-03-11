a0230833faf6a8a364950d8f07e21f39dff55469
Sasha <sashalisina@gmail.com>
2023-03-11 11:18:41 +0000

    Update sedoxil_diary.py

--- sedoxil_diary.py
+++ sedoxil_diary.py
@@ -85,26 +85,29 @@ def get_starting_day():
     :rtype: str
     """
     while True:  # getting from a user a date of starting the treatment.
-        try:
-            starting_date = input("What was the date you started to take Sedoxil?(MM/DD): 2023/ ").strip()
-            matches = re.search(r"^([0-9]{1,2})\D*([0-9]{1,2}$)", starting_date)
-            if matches:
-                if (int(matches.group(1))>12) or (int(matches.group(2))> 31):
-                    raise TypeError
-                starting_date = ('2023/' + str(matches.group(1)) + '/' + str(matches.group(2)))
+    try:
+        starting_date = input("What was the date you started to take Sedoxil?(MM/DD): 2023/ ").strip()
+        matches = re.search(r"^([0-9]{1,2})\D*([0-9]{1,2}$)", starting_date)
+        if matches:
+            print(f"matches.group(1){matches.group(1)},matches.group(2){matches.group(2)}" )
 
-                if starting_date > today:
-                    raise ValueError
-            else:
+            if (int(matches.group(1))< 12) or (int(matches.group(1)) > 0 ) and ((int(matches.group(2)) < 31) or (int(matches.group(2)) > 0)) :
+                raise ValueError
+            starting_date = ('2023/' + str(matches.group(1)) + '/' + str(matches.group(2)))
+
+            if starting_date > today:
                 raise TypeError
-        except TypeError:
-            print()
-            print("The date is not recognized, please use the decimal numbers up to 12 for months and up to 31 for days \n")
-        except ValueError:
-            print()
-            print("You wrote the date in the future as the starting date, but you can't start the diary about the future! :) Sorry \n")
         else:
-            break
+            raise ValueError
+
+    except ValueError:
+        print()
+        print("The date is not recognized, please use the decimal numbers up to 12 for months and up to 31 for days and bigger than 0 :)\n")
+    except TypeError:
+        print()
+        print("You wrote the date in the future as the starting date, but you can't start the diary about the future! :) Sorry \n")
+    else:
+        break
     return starting_date
 
 
